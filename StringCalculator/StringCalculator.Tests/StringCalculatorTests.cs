38858d0f02632c4c230707a0aa32db2c8bf15d3a
MariyaMindra <Mariya.Mindra@gmail.com>
2012-10-05 22:42:16 +0300

    kata

---
+++ StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        // for an empty string it will return 0
+        [Fact]
+        public void for_an_empty_string_it_will_return_0()
+        {
+            // Arrange
+            //var _stringCalculator = new Calculator();
+
+            //Act
+            int actual = Calculator.Add("");
+            int expected = 0;
+
+            //Assert
+            actual.Should().Be(expected);
+        }
+
+        //for string = 2 it should return 2
+        [Fact]
+        public void for_string_2_it_should_return_2()
+        {
+            //Arrange
+            //Act
+            int actual = Calculator.Add("2");
+            int expected = 2;
+
+            //Assert
+            actual.Should().Be(expected);
+        }
+
+        //for string "1,2" it should return 3
+        [Fact]
+        public void for_string_1_2_it_should_return_3()
+        {
+            int actual = Calculator.Add("1,2");
+            int expected = 3;
+
+            actual.Should().Be(expected);
+        }
+
+        //should handle new lines between numbers
+        [Fact]
+        public void handle_new_lines_between_numbers()
+        {
+            int actual = Calculator.Add("1\n2,3");
+            int expected = 6;
+
+            actual.Should().Be(expected);
+        }
+        //should support different delimiters 
+        [Fact]
+        public void support_different_delimiters()
+        {
+            int actual = Calculator.Add("//;\n1;2");
+            int expected = 3;
+            actual.Should().Be(expected);
+
+        }
+        // negative number should throw an exception “negatives not allowed” 
+        [Fact]
+        public void negative_number_should_throw_an_exception()
+        {
+            Action act = () =>Calculator.Add("-2, 2");
+            act.ShouldThrow<ArgumentException>().WithMessage("negatives not allowed");
+        }
+
+
+    }
+
+}
---
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public static int Add(string query)
+        {
+            if (query == "") return 0;
+            string[] numbers;
+            char delimiter;
+            if (query.StartsWith("//"))
+            {
+                delimiter = query.ToCharArray()[2];
+                query = query.Substring(4);
+            }
+            else delimiter = ',';
+            Console.WriteLine(query);
+            numbers = query.Split(delimiter, '\n');
+            if (numbers.Any(n => Convert.ToInt32(n) < 0)) throw new ArgumentException("negatives not allowed"); 
+            int result = numbers.Where(n=>n!="").Select(n => Convert.ToInt32(n)).Sum();
+            return result;
+
+            
+        }
+        //private static string[] Parser(string query)
+        //{
+        //    Regex reg = new Regex("^//.");
+        //    if (reg.Match(query).Success)
+        //    {
+                
+        //    }
+        //}
+    }
+}
